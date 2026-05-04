c931964a347041ef0cf542bd4ca52255845286b3
Your Name <office@gmail.com>
2026-05-04 11:05:11 +0100

    fixed

--- app/routes/auth.login/route.tsx
+++ app/routes/auth.login/route.tsx
@@ -1,18 +1,15 @@
 import { useEffect } from "react";
 import type { LoaderFunctionArgs } from "react-router";
 import { useLoaderData } from "react-router";
-import { login } from "../../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const shop =
     process.env.SHOPIFY_SHOP_DOMAIN || "nii-clean-products.myshopify.com";
 
-  const url = new URL(request.url);
-  url.searchParams.set("shop", shop);
+  const location = `/auth?shop=${shop}`;
 
- const location = `/auth?shop=${shop}`;
-
-return { location };
+  return { location };
+};
 
 export const action = loader;
 
@@ -20,20 +17,15 @@ export default function AuthLogin() {
   const { location } = useLoaderData<typeof loader>();
 
   useEffect(() => {
-    if (location) {
-      window.open(location, "_top");
-    }
+    window.open(location, "_top");
   }, [location]);
 
   return (
     <div style={{ padding: 40 }}>
       <h2>Redirecting to Shopify login...</h2>
-
-      {location && (
-        <a href={location} target="_top">
-          Click here if it does not continue
-        </a>
-      )}
+      <a href={location} target="_top">
+        Click here if it does not continue
+      </a>
     </div>
   );
 }
