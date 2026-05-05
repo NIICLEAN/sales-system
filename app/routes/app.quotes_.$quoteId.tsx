7847e0b293f7c437fb34b286196bc73f0aeeeedb
Your Name <office@gmail.com>
2026-05-05 10:44:46 +0100

    aa

--- app/routes/app.quotes_.$quoteId.tsx
+++ app/routes/app.quotes_.$quoteId.tsx
@@ -51,7 +51,7 @@ function downloadPdf() {
   link.href = pdfUrl;
   link.target = "_blank";
   link.rel = "noopener noreferrer";
-  link.download = `Quote-QUO-${quote.id}.pdf`;
+  link.download = `NCP-QUO-${quote.id}.pdf`;
 
   document.body.appendChild(link);
   link.click();
