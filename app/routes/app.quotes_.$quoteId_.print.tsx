7beb5de524de12e41d25638fbb3665b76ea1cafe
Your Name <office@gmail.com>
2026-05-05 10:26:25 +0100

    aaaq

--- app/routes/app.quotes_.$quoteId_.print.tsx
+++ app/routes/app.quotes_.$quoteId_.print.tsx
@@ -36,10 +36,11 @@ function downloadPdf(event?: React.MouseEvent<HTMLButtonElement>) {
   event?.preventDefault();
   event?.stopPropagation();
 
-  const pdfUrl =
-    window.location.pathname.replace(/\/$/, "") +
-    "/pdf" +
-    window.location.search;
+  const basePath = window.location.pathname
+    .replace(/\/print\/?$/, "") // remove /print
+    .replace(/\/$/, "");
+
+  const pdfUrl = basePath + "/pdf" + window.location.search;
 
   const link = document.createElement("a");
   link.href = pdfUrl;
